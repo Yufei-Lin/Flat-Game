d973a7239e7c12858143e6e3aa3e4da2d77a5a0b
Afeiii030 <149925598+Afeiii030@users.noreply.github.com>
2024-02-06 09:38:47 -0500

    1.2

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -35,7 +35,7 @@ public class Player : MonoBehaviour
    
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("aa");
+        //Debug.Log("aa");
 
         if (collision.name == "2")
         {
